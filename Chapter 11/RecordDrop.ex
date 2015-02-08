8a8c092efbc16a2d209be0afbf3564cef5124c55
John McCormack <mccormack.wgsi@gmail.com>
2015-02-08 00:04:11 -0500

    Half way through chapter 11

--- Chapter 11/RecordDrop.ex
+++ Chapter 11/RecordDrop.ex
@@ -1,11 +1,11 @@
 defmodule RecordDrop do
-    require Planemo
     require Tower
-    
-    def fall_velocity(t = Tower.tower()) do
-        fall_velocity(Tower.tower(t, :planemo), Tower.tower(t, :height))
+    import Tower
+    def fall_velocity(t = tower(planemo: planemo, height: distance)) do
+        IO.puts("From #{tower(t, :name)}'s elevation of #{distance} meters on #{planemo}, ")
+        IO.puts("the object will reach #{fall_velocity(planemo, distance)} m/s")
+        IO.puts("before crashing in #{tower(t, :location)}")
     end
-    
     def fall_velocity(:earth, distance) when distance >= 0 do
         :math.sqrt(2 * 9.8 * distance)
     end
